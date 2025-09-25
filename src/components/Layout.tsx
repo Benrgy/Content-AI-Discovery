1613e05f138175beb8c164d7ea9a6f483658f23a
[dyad] <bengrauwde@hotmail.com>
2025-09-26 01:38:46 +0200

    [dyad] Improved layout flexibility - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import React from "react";
-import { Outlet } from "react-router-dom"; // Import Outlet
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import { MadeWithDyad } from "./made-with-dyad";
 
-const Layout: React.FC = () => { // Removed LayoutProps interface and children prop
+const Layout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow flex flex-col items-center py-8">
-        <Outlet /> {/* Render nested route content here */}
+      <main className="flex-grow flex flex-col"> {/* Removed items-center and py-8 */}
+        <Outlet />
       </main>
       <footer className="py-4">
         <MadeWithDyad />
