c85e2dc840d0da401e0da584c174d0066344d2d6
[dyad] <bengrauwde@hotmail.com>
2025-09-26 00:24:53 +0200

    [dyad] Moved Toaster to Layout component - wrote 2 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Header from "./Header"; // Import the Header component
 import { MadeWithDyad } from "./made-with-dyad"; // Import MadeWithDyad
+import { Toaster as Sonner } from "@/components/ui/sonner"; // Import Toaster
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -12,12 +13,13 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header /> {/* Include the Header here */}
-      <main className="flex-grow container mx-auto px-4 py-8">
+      <main className="flex-grow flex flex-col items-center py-8"> {/* Adjusted styling for flexibility */}
         {children}
       </main>
       <footer className="py-4">
         <MadeWithDyad />
       </footer>
+      <Sonner /> {/* Include the Toaster here */}
     </div>
   );
 };
