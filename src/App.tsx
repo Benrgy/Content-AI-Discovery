d1193353e0f8b35319a2a18afc74d01e21b7b6a9
[dyad] <bengrauwde@hotmail.com>
2025-09-26 00:20:23 +0200

    [dyad] Integrated Header into Layout component - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import { appRoutes } from "./routes"; // Import appRoutes
 
 const queryClient = new QueryClient();
@@ -14,14 +13,12 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <Routes>
-            {appRoutes.map((route) => (
-              <Route key={route.path} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-        </div>
+        {/* The Header is now part of the Layout component, so we remove it from here */}
+        <Routes>
+          {appRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Header from "./Header"; // Import the Header component
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,9 +9,12 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <main className="flex-grow container mx-auto px-4 py-8 min-h-[calc(100vh-4rem)]">
-      {children}
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <Header /> {/* Include the Header here */}
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {children}
+      </main>
+    </div>
   );
 };
 
