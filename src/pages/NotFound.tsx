23ea938630fc4d66f69814718903fad8c0f9c1d5
[dyad] <bengrauwde@hotmail.com>
2025-09-26 01:55:54 +0200

    [dyad] Improved 404 page consistency - wrote 1 file(s)

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button"; // Import Button
 
 const NotFound = () => {
   const location = useLocation();
@@ -15,10 +16,12 @@ const NotFound = () => {
     <div className="flex flex-col items-center justify-center flex-grow rounded-lg p-6">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 dark:text-gray-300 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
-        </a>
+        <p className="text-xl text-gray-600 dark:text-gray-300 mb-6">Oops! Page not found</p>
+        <Button asChild> {/* Use Button as a child of Link */}
+          <Link to="/">
+            Return to Home
+          </Link>
+        </Button>
       </div>
     </div>
   );
