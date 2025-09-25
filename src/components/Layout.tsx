79481d4de71b675613c888ee216567e73e43d6b0
[dyad] <bengrauwde@hotmail.com>
2025-09-26 01:37:44 +0200

    [dyad] Updated layout to use React Router Outlet - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,16 @@
 "use client";
 
 import React from "react";
+import { Outlet } from "react-router-dom"; // Import Outlet
 import Header from "./Header";
 import { MadeWithDyad } from "./made-with-dyad";
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC = () => { // Removed LayoutProps interface and children prop
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow flex flex-col items-center py-8">
-        {children}
+        <Outlet /> {/* Render nested route content here */}
       </main>
       <footer className="py-4">
         <MadeWithDyad />
