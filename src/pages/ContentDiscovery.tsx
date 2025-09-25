3fbbe85ef2b3e749ec84febec6a8c4c04b41ba48
[dyad] <bengrauwde@hotmail.com>
2025-09-26 00:18:51 +0200

    [dyad] Removed redundant search button from content discovery - wrote 1 file(s)

--- src/pages/ContentDiscovery.tsx
+++ src/pages/ContentDiscovery.tsx
@@ -121,7 +121,6 @@ const ContentDiscovery = () => {
             </span>
           )}
         </Button>
-        <Button onClick={() => setAppliedPlatforms(selectedPlatforms)}>Search</Button> {/* Re-apply filters on search button click */}
       </div>
 
       <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
