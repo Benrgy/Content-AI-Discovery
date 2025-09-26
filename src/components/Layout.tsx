35d7cc469a1bb6b10a2bb8ca9773913ccb5e9ace
[dyad] <bengrauwde@hotmail.com>
2025-09-26 02:13:17 +0200

    [dyad] Made layout main content area more flexible - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC = () => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow flex flex-col">
-        <div className="container mx-auto py-8 px-4 flex-grow flex flex-col"> {/* Centralized container and flex-grow */}
+        <div className="flex-grow flex flex-col"> {/* Removed container mx-auto py-8 px-4 */}
           <Outlet />
         </div>
       </main>
