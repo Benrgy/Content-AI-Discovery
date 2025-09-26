45b21eecd7c587975d7cf1dad3fcb9cfecdec249
[dyad] <bengrauwde@hotmail.com>
2025-09-26 02:33:28 +0200

    [dyad] Refactored Layout component - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import ScrollToTopButton from "./ScrollToTopButton";
 
-const Layout: React.FC = () => {
+const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
